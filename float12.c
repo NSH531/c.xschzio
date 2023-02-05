d3eda6814fc3f8d440cf917b7097c6ae6ca43b9a
NSH531 <NSH531@GMAIL.COM>
2023-02-05 15:40:24 +0200

    Update float12.c

--- float12.c
+++ float12.c
@@ -42,6 +42,69 @@ float12 float12_mul(float12 a, float12 b) {
 float12 float12_div(float12 a, float12 b) {
   return float_to_float12(float12_to_float(a) / float12_to_float(b));
 }
+#include <math.h>
+
+typedef union {
+  struct {
+    unsigned fraction:6;
+    unsigned exponent:5;
+    unsigned sign:1;
+  } parts;
+  unsigned short value;
+} float12;
+
+// Convert a 32-bit float to a 12-bit float
+float12 float_to_float12(float f) {
+  float12 result;
+  result.parts.sign = (f < 0);
+  result.parts.exponent = (unsigned char)(fabs(f) / 2);
+  result.parts.fraction = (unsigned char)(fabs(f) * 64) & 63;
+  return result;
+}
+
+// Convert a 12-bit float to a 32-bit float
+float float12_to_float(float12 f12) {
+  return (f12.parts.sign ? -1 : 1) * (f12.parts.fraction / 64.0 + f12.parts.exponent * 2);
+}
+
+// Raise x to the power of n
+float12 float12_pow(float12 x, int n) {
+  float12 result = float_to_float12(1);
+  for (int i = 0; i < n; i++) {
+    result = float12_mul(result, x);
+  }
+  return result;
+}
+
+// Compute the factorial of n
+float12 float12_factorial(int n) {
+  float12 result = float_to_float12(1);
+  for (int i = 2; i <= n; i++) {
+    result = float12_mul(result, float_to_float12(i));
+  }
+  return result;
+}
+
+// Approximate the hyperbolic tangent of x using a Taylor series
+float12 float12_tanh(float12 x) {
+  float12 result = x;
+  float12 x_pow = x;
+  float12 n_factorial = float_to_float12(1);
+  for (int n = 1; fabs(float12_to_float(x_pow)) > 1e-6; n++) {
+    x_pow = float12_mul(x_pow, x);
+    x_pow = float12_mul(x_pow, x);
+    n_factorial = float12_mul(n_factorial, float_to_float12(2 * n));
+    n_factorial = float12_mul(n_factorial, float_to_float12(2 * n + 1));
+    result = result + float12_div(x_pow, n_factorial);
+  }
+  return result;
+}
+
+int main(void) {
+  float12 x = float_to_float12(1);
+  printf("tanh(%f) = %f\n", float12_to_float(x), float12_to_float(float12_tanh(x)));
+  return 0;
+}
 
 int main(void) {
   float12 a = float_to_float12(2.75);
