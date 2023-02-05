db74790c971e504fed05b286c631521646b5bb3a
NSH531 <NSH531@GMAIL.COM>
2023-02-05 15:35:43 +0200

    Update float12.c

--- float12.c
+++ float12.c
@@ -1,20 +1,54 @@
 #include <stdio.h>
 
-typedef struct {
-    unsigned short fraction: 6;
-    unsigned short exponent: 5;
-    unsigned short sign: 1;
+typedef union {
+  struct {
+    unsigned fraction:6;
+    unsigned exponent:5;
+    unsigned sign:1;
+  } parts;
+  unsigned short value;
 } float12;
 
-float12 add(float12 a, float12 b) {
-    // Implementation of addition operation
-    // ...
+// Convert a 32-bit float to a 12-bit float
+float12 float_to_float12(float f) {
+  float12 result;
+  result.parts.sign = (f < 0);
+  result.parts.exponent = (unsigned char)(fabs(f) / 2);
+  result.parts.fraction = (unsigned char)(fabs(f) * 64) & 63;
+  return result;
 }
 
-int main() {
-    float12 a, b, c;
-    // Initialize a and b
-    // ...
-    c = add(a, b);
-    return 0;
+// Convert a 12-bit float to a 32-bit float
+float float12_to_float(float12 f12) {
+  return (f12.parts.sign ? -1 : 1) * (f12.parts.fraction / 64.0 + f12.parts.exponent * 2);
 }
+
+// Add two 12-bit floats
+float12 float12_add(float12 a, float12 b) {
+  return float_to_float12(float12_to_float(a) + float12_to_float(b));
+}
+
+// Subtract two 12-bit floats
+float12 float12_sub(float12 a, float12 b) {
+  return float_to_float12(float12_to_float(a) - float12_to_float(b));
+}
+
+// Multiply two 12-bit floats
+float12 float12_mul(float12 a, float12 b) {
+  return float_to_float12(float12_to_float(a) * float12_to_float(b));
+}
+
+// Divide two 12-bit floats
+float12 float12_div(float12 a, float12 b) {
+  return float_to_float12(float12_to_float(a) / float12_to_float(b));
+}
+
+int main(void) {
+  float12 a = float_to_float12(2.75);
+  float12 b = float_to_float12(1.25);
+  printf("a: %f\n", float12_to_float(a));
+  printf("b: %f\n", float12_to_float(b));
+  printf("a + b: %f\n", float12_to_float(float12_add(a, b)));
+  printf("a - b: %f\n", float12_to_float(float12_sub(a, b)));
+  printf("a * b: %f\n", float12_to_float(float12_mul(a, b)));
+  printf("a / b: %f\n", float12_to_float(float12_
